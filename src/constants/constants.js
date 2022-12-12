bede35bf0cfdc63906c5c87f083e3fdc57645d9a
Sneha <snhthakare@gmail.com>
2022-12-12 19:59:38 +0100

    Adds project data file

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'JERA JAPAN',
+    description: "JERA is the largest power generation company in Japan. This web and mobile app is used to monitor the power generation equipments",
+    image: '/images/1.png',
+    tags: ['React', 'Typescript', 'Node.js'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Car Marketplace',
+    description: "This is a one stop all website for car rental service, rent a car, book cars, drivers,pick up trucks to suit all your needs",
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript', 'HTML5', 'MUI'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Church Website',
+    description: "Online website for your church, offer prayers,seek talks from priests, help the needy",
+    image: '/images/3.jpg',
+    tags: ['Ruby', 'Rails', 'React', 'Javascript'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Covid Tracker',
+    description: "This app tracks covid cases from all across the world, get data region wise or check the status in your area",
+    image: '/images/4.jpg',
+    tags: ['React', 'Javascript', 'MUI'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
