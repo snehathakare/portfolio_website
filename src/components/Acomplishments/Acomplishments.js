ecb3873fea0cddca1ccd8b249b9800c585ff62fb
Sneha <snhthakare@gmail.com>
2022-12-12 20:31:23 +0100

    Adds accompolishment component

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,23 @@
 import React from 'react';
-
+import { data } from './../../constants/constants'
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
-const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
-];
+
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionDivider />
+    <SectionTitle>Accompolishments</SectionTitle>
+    <Boxes>
+      {data.map((item) => (
+        <Box>
+          <BoxNum>{item.number}</BoxNum>
+          <BoxText>{item.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
