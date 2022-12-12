c50f6416f8731163f19c67a4f0143336834cf8ee
Sneha <snhthakare@gmail.com>
2022-12-12 20:07:08 +0100

    Adds technologies components

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,10 +3,33 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section>
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>I have worked on many technologies from frontend to backend</SectionText>
+    <List>
+      <ListItem>
+        <ListContainer>
+          <ListTitle>Frontend</ListTitle>
+          <ListParagraph>Experience with <br /></ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <ListContainer>
+          <ListTitle>Backend</ListTitle>
+          <ListParagraph>Experience with <br /></ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <ListContainer>
+          <ListTitle>Tools</ListTitle>
+          <ListParagraph>Experience with <br /></ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+
+  </Section>
 );
 
 export default Technologies;
