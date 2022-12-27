3dd45928d426350a7e77e7d120d826ebcbd96a11
Sneha <snhthakare@gmail.com>
2022-12-27 21:28:31 +0100

    Removes unwanted import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
 import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
+import Resume from '../components/Resume/Resume';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
 const Home = () => {
   return (
     <Layout>
-      {/* <Section grid> */}
       <Hero />
-      {/* <BgAnimation /> */}
-      {/* </Section> */}
       <Technologies />
+      <Resume />
       <Projects />
     </Layout>
   );
