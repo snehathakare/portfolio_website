b909131d5ca7b496418c1c51a4424e956736ef4b
Sneha <snhthakare@gmail.com>
2023-01-03 19:18:19 +0100

    Updates content for website

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,48 +4,32 @@ export const projects = [
     description: "JERA is the largest power generation company in Japan. This web and mobile app is used to monitor the power generation equipments",
     image: '/images/1.png',
     tags: ['React', 'Typescript', 'Node.js'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
     id: 0,
   },
   {
     title: 'Car Marketplace',
-    description: "This is a one stop all website for car rental service, rent a car, book cars, drivers,pick up trucks to suit all your needs",
+    description: "This is a website for car sharing marketplace, rent a car, book a ride with drivers, be a host to share your car.",
     image: '/images/2.png',
     tags: ['React', 'JavaScript', 'HTML5', 'MUI'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
     id: 1,
   },
   {
     title: 'Church Website',
-    description: "Online website for your church, offer prayers,seek talks from priests, help the needy",
+    description: "Online community website for church. Users can offer prayers, worship, help the needy through this website",
     image: '/images/3.jpg',
     tags: ['Ruby', 'Rails', 'React', 'Javascript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
     id: 2,
   },
   {
     title: 'Covid Tracker',
-    description: "This app tracks covid cases from all across the world, get data region wise or check the status in your area",
+    description: "This app tracks covid cases from all across the world, get data region wise and check the status in your area",
     image: '/images/4.jpg',
     tags: ['React', 'Javascript', 'MUI'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
+    id: 3,
   },
 ];
 
-export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
-];
-
-export const data = [
+export const skills = [
   { title: 'ReactJs', logo: '/images/react.png' },
   { title: 'NextJs', logo: '/images/next.png' },
   { title: 'Typescript', logo: '/images/typescript.png' },
@@ -55,9 +39,9 @@ export const data = [
   { title: 'Tailwind', logo: '/images/mui.png' },
 ];
 
-export const articles = [
-  { src: 'https://snhthakare.medium.com/how-can-you-mitigate-carbon-footprint-as-a-developer-14b35e41e97a', text: 'HTML5' },
-  { src: 'https://snhthakare.medium.com/simple-ways-to-connect-your-react-app-to-metamask-fee4f99b3d37', text: 'Next Js', },
-  { src: 'https://snhthakare.medium.com/clean-coding-practices-i-use-in-react-82b6f7f17022', text: 'React Js', },
-  { src: 'https://snhthakare.medium.com/easy-ways-to-deploy-your-react-app-for-free-c75219841486', text: 'something' }
+export const hobbies = [
+  { src: '/images/Photo1.jpg' },
+  { src: '/images/Photo2.jpg' },
+  { src: '/images/Photo3.jpg' },
+  { src: '/images/Photo4.jpg' }
 ]
