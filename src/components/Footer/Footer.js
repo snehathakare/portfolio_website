0c789dbfb2e13b5cf274e9286d2978e5770f3ace
Sneha <snhthakare@gmail.com>
2023-01-04 18:46:44 +0100

    Updates footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { SocialIcons } from '../Header/HeaderStyles';
-import { FooterWrapper, LinkColumn, LinkItem, LinkList } from './FooterStyles';
+import { FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle } from './FooterStyles';
 
 const Footer = () => {
   return (
     <FooterWrapper>
       <LinkList>
-      <LinkColumn>
+        <LinkColumn>
           <LinkItem>
-            Let's stay connected
+            <LinkTitle>
+              Let's stay connected
+            </LinkTitle>
+          </LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkItem>
+            Although I’m not looking for any new opportunities currently, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!
           </LinkItem>
         </LinkColumn>
         <LinkColumn>
@@ -18,19 +25,19 @@ const Footer = () => {
           </LinkItem>
         </LinkColumn>
         <LinkColumn>
-        <LinkItem>
-          <SocialIcons>
-            <AiFillGithub />
-          </SocialIcons>
-          <SocialIcons>
-            <AiFillInstagram />
-          </SocialIcons>
-          <SocialIcons>
-            <AiFillLinkedin />
-          </SocialIcons>
-        </LinkItem>
+          <LinkItem>
+            <SocialIcons>
+              <AiFillGithub />
+            </SocialIcons>
+            <SocialIcons>
+              <AiFillInstagram />
+            </SocialIcons>
+            <SocialIcons>
+              <AiFillLinkedin />
+            </SocialIcons>
+          </LinkItem>
         </LinkColumn>
-      </LinkList>  
+      </LinkList>
     </FooterWrapper>
   );
 };
