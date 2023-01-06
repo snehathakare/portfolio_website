cf5d889be7ebc6bac348e130f4224f63b39eab1c
Sneha <snhthakare@gmail.com>
2023-01-06 18:13:04 +0100

    Updates header links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,51 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCode, DiCssdeck } from 'react-icons/di';
-
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+import { DiCode } from 'react-icons/di';
+import { Section } from '../../styles/GlobalComponents'
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () => (
-  <Container>
-    <Div1>
-      <Link href="/">
-        <a style={{ display: 'flex', alignItems: 'center', color: "white" }}>
-          <DiCode size="3rem" />
-          <span>Portfolio</span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href="#projects">
-          <NavLink>Projects</NavLink>
+  <Section>
+    <Container>
+      <Div1>
+        <Link href="/">
+          <a style={{ display: 'flex', alignItems: 'center', color: "white" }}>
+            <DiCode size="4rem" />
+          </a>
         </Link>
-      </li>
-      <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#about">
-          <NavLink>About</NavLink>
-        </Link>
-      </li>
-    </Div2>
-    <Div3>
-      <SocialIcons>
-        <AiFillGithub />
-      </SocialIcons>
-      <SocialIcons>
-        <AiFillInstagram />
-      </SocialIcons>
-      <SocialIcons>
-        <AiFillLinkedin />
-      </SocialIcons>
-    </Div3>
-  </Container>
+      </Div1>
+      <Div2>
+        <li>
+          <NavLink href="https://drive.google.com/file/d/10vUAIvXtrdNgQhywdViHCQ3pFHt8o159/view?usp=sharing" target="_blank">
+            Resume
+          </NavLink>
+        </li>
+        <li>
+          <NavLink href="https://medium.com/@snhthakare" target="_blank">
+            Blog
+          </NavLink>
+        </li>
+        <li>
+          <NavLink href="/articles">
+            My story
+          </NavLink>
+        </li>
+      </Div2>
+      <Div3>
+        <SocialIcons>
+          <AiFillGithub />
+        </SocialIcons>
+        <SocialIcons>
+          <AiFillInstagram />
+        </SocialIcons>
+        <SocialIcons>
+          <AiFillLinkedin />
+        </SocialIcons>
+      </Div3>
+    </Container>
+  </Section>
+
 );
 
 export default Header;
