524da840b22734a3d7565aac94adf6b359ea38ed
Sneha <snhthakare@gmail.com>
2023-01-06 18:23:21 +0100

    Renames article page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import About from '../components/About/About';
+import { Layout } from '../layout/Layout';
+
+const about = () => {
+  return (
+    <Layout>
+      <About />
+    </Layout>
+  );
+};
+
+export default about;
