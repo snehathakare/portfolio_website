25a7ba6db96fbec2cac50e41822e78034322fc53
Sneha <snhthakare@gmail.com>
2023-01-13 23:07:05 +0530

    Adds skills component to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,13 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
-import Resume from '../components/Resume/Resume';
+import Skills from '../components/Skills/Skills';
 import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
 
 const Home = () => {
   return (
     <Layout>
       <Hero />
-      <Technologies />
-      <Resume />
+      <Skills />
       <Projects />
     </Layout>
   );
